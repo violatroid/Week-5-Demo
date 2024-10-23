b8be432665ab34dd4daafd49804675a20223cf7b
Yun Johnson <violatroid@gmail.com>
2024-10-23 16:01:02 -0400

    updated 3

---
+++ food_menu_.js
@@ -0,0 +1,23 @@
+
+let menuObject = {
+    drinkList: ["Water", "Tea", "Sweettea", "Coke", "Dr. Pepper", "sprite"],
+    entreeList: ["Hamburger w/ fries", "Grilled Cheese", ]
+    dessertList: ["cheesecake", "chocolate cake", "Snickerdoodle Cookie"]
+};
+
+//console.log(menuObject.drinkList)
+
+for(key in menuObject){
+    let list = menuObject[key];
+    console.log(key, list.length);
+    //console.log(list[0])
+    for(let i = 0; i < list.length; i++){
+        const.text = list[i];
+        console.log("\t\t", i, text); // "\t" - back slash "t" is for tab but for hrere we added two tabs
+        let i = 0;
+        for(text of list){
+            confirm.log("\t\t", i, text);
+            i++;
+        }
+    }
+}
